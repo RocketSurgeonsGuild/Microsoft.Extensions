8f37dec9878f8a3e16bc2d8e6bf7219d6e6bdfc6
David Driscoll <david.driscoll@gmail.com>
2019-06-03 10:29:45 -0400

    Added WebJobsBuilder conventions

---
+++ src/WebJobs.Abstractions/IWebJobsConvention.cs
@@ -0,0 +1,12 @@
+﻿using Rocket.Surgery.Conventions;
+
+namespace Rocket.Surgery.Extensions.WebJobs
+{
+    /// <summary>
+    /// Interface IWebJobsConvention
+    /// </summary>
+    /// TODO Edit XML Comment Template for IWebJobsConvention
+    public interface IWebJobsConvention : IConvention<IWebJobsConventionContext>
+    {
+    }
+}
